33914c1606d73c68e32a9e77524f3eb513cef78e
wvdp1992 <63849892+wvdp1992@users.noreply.github.com>
2022-11-22 16:53:02 +0100

    Update streamlit_app.py
    Choose a Few Fruits to Set a Good Example

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list,['Avocado','Strawberries'])
